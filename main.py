4c785e09a586b135757edbd7f24fddac1badfe9a
JinheonBaek <Jinheon.Baek@outlook.kr>
2018-11-20 13:36:06 +0900

    Make init structure that guide this software

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+"""
+Init function that prints guide of this software.
+"""
+def init():
+    print("Guide")
+
+def main():
+    init()
+    print("Hello world")
+
+if __name__ == '__main__':
+    main()
