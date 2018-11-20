4499e1547eab33198f35d930453cbab30f5746d6
JinheonBaek <Jinheon.Baek@outlook.kr>
2018-11-20 14:08:56 +0900

    Add HomeAppliance class and many appliance classes that inherit HomeAppliance class

--- main.py
+++ main.py
@@ -1,12 +1,76 @@
 """
-Init function that prints guide of this software.
+Guide function that prints guideline of this software.
 """
-def init():
+def guide():
     print("Guide")
 
+class HomeAppliance(object):
+    def __init__(self):
+        self._status = None
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+    
+class WashingMachine(HomeAppliance):
+    def __init__(self):
+        self._status = None
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+
+class AirConditioner(HomeAppliance):
+    def __init__(self):
+        self._status = None
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+
+class Boiler(HomeAppliance):
+    def __init__(self):
+        self._status = None
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+
+class AirCleaner(HomeAppliance):
+    def __init__(self):
+        self._status = None
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+
 def main():
-    init()
+    guide()
     print("Hello world")
+    test = Boiler()
+    test.status = '1'
+    print(test.status)
+
 
 if __name__ == '__main__':
     main()
